2771accf0d485b7b789680cc8e0c2461bbd6cc5e
ayoub <ayoubbenhamou0731@gmail.com>
2021-06-01 22:35:54 +0100

    add readme.md

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ superUser+="group1 = Group.objects.get(name = 'doctorant');"
 superUser+="group2 = Group.objects.get(name = 'admin');"
 
 # user 1
-superUser+="ayoub = UserAccount.objects.create_user(email='ayoub@gmail.com', username='user_1', first_name='ayoub', last_name='ben hamou', password='ase123@$');"
+superUser+="omar = UserAccount.objects.create_user(email='omar@gmail.com', username='user_1', first_name='omar', last_name='jed', password='omarjed');"
 
 superUser+="rachid = UserAccount.objects.create_user(email='rachid@gmail.com', username='user_2', first_name='rachid', last_name='ben hamou', password='ase123@$');"
 
@@ -16,7 +16,7 @@ superUser+="sara = UserAccount.objects.create_user(email='sara@gmail.com', usern
 
 superUser+="salma = UserAccount.objects.create_user(email='salma@gmail.com', username='user_4', first_name='salma', last_name='Azzouzi', password='ase123@$');"
 
-superUser+="ayoub.save();"
+superUser+="omar.save();"
 superUser+="rachid.save();"
 superUser+="sara.save();"
 superUser+="salma.save();"
@@ -24,15 +24,15 @@ superUser+="salma.save();"
 superUser+="EncadrantModel(user = salma).save();"
 superUser+="EncadrantModel(user = sara).save();"
 
-superUser+="DoctorantModel(user = ayoub, apogee = '18005544', cin = 'GM215420').save();"
+superUser+="DoctorantModel(user = omar, apogee = '18005544', cin = 'GM215420').save();"
 superUser+="DoctorantModel(user = rachid, apogee = '18005544', cin = 'GM215420').save();"
 
-superUser+="MemberModel(user = ayoub, email = ayoub.email, userType = 1).save();"
+superUser+="MemberModel(user = omar, email = omar.email, userType = 1).save();"
 superUser+="MemberModel(user = rachid, email = rachid.email, userType = 1).save();"
 superUser+="MemberModel(user = sara, email = sara.email, userType = 0).save();"
 superUser+="MemberModel(user = salma, email = salma.email, userType = 0).save();"
 
-superUser+="ayoub.groups.add(group1);"
+superUser+="omar.groups.add(group1);"
 superUser+="rachid.groups.add(group1);"
 superUser+="sara.groups.add(group0);"
 superUser+="salma.groups.add(group0);"
--- readme.md
+++ readme.md
@@ -1,19 +1,57 @@
-for install the project
+# pour installer le projet 
+
+créer un dossier pour votre projet
+ouvrir un **bash** terminal 
+utilisez **cmder** ou n'importe quel terminal bash 
+to download cmder : https://cmder.net/
+
+# bien sûr, vous devez installer python v3 >
+
+# utilisez git ou téléchargez simplement le projet depuis  https://github.com/ayoubHam2000/Lab-management.git
 
 git init 
 git remote add origin https://github.com/ayoubHam2000/Lab-management.git
 git pull origin master
 
+# installer des bibliothèques 
 
 py -m pip install virtualenv
 virtualenv env
 activate
 py -m pip install -r requirement.txt
 pip install -U python-dotenv
+py -m pip install django-widget-tweaks
 
 create .env file for email server
 copy and past these lines to .env
 
+# les configuration de serveur d'email
 export EMAIL_HOST=smtp.gmail.com
-export EMAIL_HOST_USER=ayoub93527152@gmail.com
-export EMAIL_HOST_PASSWORD=aqwpmnfgh14785
+export EMAIL_HOST_USER=gmail
+export EMAIL_HOST_PASSWORD=gmail_password
+
+
+# pour exécuter et configurer le serveur pour la première fois 
+bash reset.sh 
+# pour exécuter le serveur ultérieurement
+py manage.py runserver
+
+
+**you can change the password and email of the super admin in reset.sh --21**
+
+pour utiliser le vrai serveur de email, vous devez commenter cette ligne sur  labManagement/settings.py
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+et libérez ces lignes 
+# config = dotenv_values(".env")
+# EMAIL_HOST = config['EMAIL_HOST']
+# EMAIL_HOST_USER = config['EMAIL_HOST_USER']
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
+# EMAIL_HOST_PASSWORD = config['EMAIL_HOST_PASSWORD']
+# DEFAULT_FROM_EMAIL = config['EMAIL_HOST_USER']
+
+sinon l'e-mail sera affiché dans le terminal 
+
+
